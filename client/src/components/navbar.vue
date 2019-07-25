fbe525751fb22724624e139bb261bd87e672afac
yudawardana <yuda.wardana40@gmail.com>
2019-07-25 14:15:49 +0700

    component navbar component landing

---
+++ client/src/components/navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="navbar">
+    <h3>ini header</h3>
+    <el-form>
+      <el-input placeholder="Search item..." v-model="searchBar" class="search-bar-input">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </el-form>
+    <div class="login-register-button">
+      <a class="signin" @click="signIn">Login</a>
+      <a class="signup" @click="signUp">Register</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchBar: ""
+    };
+  },
+  methods: {
+    signIn() {
+      console.log("menuju halaman login");
+    },
+    signUp() {
+      console.log("menuju halaman register");
+    }
+  }
+};
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid grey;
+}
+.login-register-button {
+  display: flex;
+}
+.signin {
+  margin-right: 20px;
+}
+.search-bar-input {
+  width: 380px;
+}
+</style>
---
+++ client/src/views/Landing.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-container class="landing">
+    <el-header height="80px">
+      <navbar />
+    </el-header>
+    <el-main>
+      <div class="hero"></div>
+      <div class="mainpage">
+        <el-row>
+          <el-col :span="8">
+            <div class="grid-content bg-purple"></div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content bg-purple-light"></div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content bg-purple"></div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import navbar from "@/components/navbar";
+
+export default {
+  components: {
+    navbar
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
